aecde5cae591a5d1587c64925cfdc36dc811a5d8
Andy Hurley <andy@itsallnice-digital.co.uk>
2013-07-11 14:26:17 +0100

    Added validation to general health profile

--- app/Plugin/StandardProfileModule/Model/GeneralHealth.php
+++ app/Plugin/StandardProfileModule/Model/GeneralHealth.php
@@ -31,4 +31,55 @@ class GeneralHealth extends StandardProfileModuleAppModel {
 			'order' => ''
 		)
 	);
+	
+	public $validate = array(
+			'general_health' => array(
+				'numeric' => array(
+					'rule' => array('numeric'),
+					'message' => 'You must record a health score',
+					'allowEmpty' => true,
+					'required' => false
+				),
+				'range' => array(
+					'rule' => array('range', -1, 6),
+					'message' => 'Please select a valid option'
+				),
+			),
+			'sickness_absence' => array(
+				'numeric' => array(
+						'rule' => array('numeric'),
+						'message' => 'Please record the number of days off work for health reasons',
+						'allowEmpty' => true,
+						'required' => false
+				),
+				'range' => array(
+						'rule' => array('range', -1, 184),
+						'message' => 'Please record the number of days off work for health reasons (between 0 and 183 days)'
+				),
+			),
+			'sickness_absence_spells' => array(
+				'numeric' => array(
+						'rule' => array('numeric'),
+						'message' => 'Please record the number of spells of sickness absence',
+						'allowEmpty' => true,
+						'required' => false
+				),
+				'range' => array(
+						'rule' => array('range', -1, 27),
+						'message' => 'Please record the number of spells of sickness absence (between 0 and 26)'
+				),
+			),
+			'work_performance' => array(
+				'numeric' => array(
+						'rule' => array('numeric'),
+						'message' => 'Please rate your work performance',
+						'allowEmpty' => true,
+						'required' => false
+				),
+				'range' => array(
+						'rule' => array('range', -1, 11),
+						'message' => 'Please rate your work performance (between 0 and 10)'
+				),
+			),
+	);
 }
